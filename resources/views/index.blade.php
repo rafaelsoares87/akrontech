056ccc2cb6bb1ee5791af245c007b885d3b27ef0
Rafael Soares <rafael.soaresoliveira87@gmail.com>
2022-08-14 18:58:22 +0000

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+
+@extends('layouts')
+@section('content')
+    <h1 class="nav-link-2">Olá</h1>
+@endsection
---
+++ resources/views/layouts/site/footer.blade.php
@@ -0,0 +1,63 @@
+<footer>
+    <div class="footer " style="background-color: #421b9b ">
+        <div class="container m-auto">
+            <div class="text-white pt-4 pb-3">
+                <div class="row ">
+                    <div class="col-md-3 col-12 mx-0 px-0">
+                        <div class="">
+
+                            <ul class="">
+                                <li class="list-group-item">
+                                    <h3>Teste</h3>    
+                                </li><br>
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                                <li class="list-group-item">A third item</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-12 col-12 mx-0 px-0">
+                        <div class="">
+
+                            <ul class="">
+                                <li class="list-group-item">
+                                    <h3>Teste</h3>    
+                                </li><br>
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                                <li class="list-group-item">A third item</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-12 col-12 mx-0 px-0">
+                        <div class="">
+
+                            <ul class="">
+                                <li class="list-group-item">
+                                    <h3>Teste</h3>    
+                                </li><br>
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                                <li class="list-group-item">A third item</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-12 col-12 mx-0 px-0">
+                        <div class="">
+
+                            <ul class="">
+                                <li class="list-group-item">
+                                    <h3>Teste</h3>    
+                                </li><br>
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                                <li class="list-group-item">A third item</li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
